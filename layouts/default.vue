37a7fcf85eb9f92336c8a323c4e34abe80edbb07
Marcos Nolasco <marcosabn@gmail.com>
2021-10-25 11:39:13 -0300

    navegaçao em rotas inteligentes

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <ul>
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/sobre">Sobre</NuxtLink>
+    </ul>
+    <Nuxt />
+    <footer>Rodapé</footer>
+  </div>
+</template>
+
+<style scoped>
+a.nuxt-link-active {
+  font-weight: bold;
+}
+</style>
