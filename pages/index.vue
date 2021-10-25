ddc9044cc40e103dbbecd4cbeb7ad9e4d722183a
Marcos Nolasco <marcosabn@gmail.com>
2021-10-25 13:22:48 -0300

    Server Side Rendering

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,29 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+
+    <h2>{{ title }}</h2>
+    <p>{{ content }}</p>
   </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
+const sleep = () => {
+  return new Promise((resolve) => setTimeout(resolve, 1500))
+}
 export default Vue.extend({
   layout: 'main',
-  mounted() {
-    const users = this.$axios.$get('/api')
-    console.log(users)
+  async asyncData() {
+    await sleep()
+    const title = 'Titulo da postagem'
+    const content = 'Corpo da postagem'
+    return { title, content }
+  },
+  data() {
+    return {
+      title: '',
+      content: ''
+    }
   }
 })
 </script>
