e63756583c68f1d2cb2917fd26d9b7cc069aad0d
Marcos Nolasco <marcosabn@gmail.com>
2021-11-03 11:25:50 -0300

    first version of vuex w ts configured

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,25 @@
 <template>
-  <HomeTemplate />
+  <!-- <HomeTemplate /> -->
+  <div class="count">
+    <h1>{{ $count }}</h1>
+    <button @click="increment(5)">acrescentar</button>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { books } from '@/store'
 export default Vue.extend({
-  layout: 'ibook'
+  layout: 'ibook',
+  computed: {
+    $count() {
+      return books.$count
+    }
+  },
+  methods: {
+    increment(number: number) {
+      books.increment(number)
+    }
+  }
 })
 </script>
